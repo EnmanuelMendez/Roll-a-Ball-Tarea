82e96c60c6dfcf7d304fe2936df0e2fb64c2fc34
Enmanuel J. Méndez Guzmán <eguzman0299@gmail.com>
2026-06-15 09:21:47 -0400

    cambios en la escena principal y creación de orquestador

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public float tiempoInicial = 60f;  // 60 segundos para el nivel
+	private float tiempoActual;
+
+	public Text textoReloj;       // Arrastra el Text del UI del reloj aquí
+	public Text textoVictoria;    // Arrastra el Text "¡Ganaste!" aquí
+
+	private bool juegoTerminado = false;
+
+	void Start()
+	{
+		tiempoActual = tiempoInicial;
+		textoVictoria.gameObject.SetActive(false);
+		Time.timeScale = 1f;
+	}
+
+	void Update()
+	{
+		if (juegoTerminado) return;
+
+		tiempoActual -= Time.deltaTime;
+		textoReloj.text = "Tiempo: " + Mathf.Max(0, Mathf.CeilToInt(tiempoActual)).ToString();
+
+		if (tiempoActual <= 0f && !juegoTerminado)
+		{
+			PerderJuego();
+		}
+	}
+
+	public void GanarJuego()
+	{
+		if (juegoTerminado) return;
+		juegoTerminado = true;
+		textoVictoria.gameObject.SetActive(true);
+		Time.timeScale = 0f;
+		Invoke("RegresarAlMenuPrincipal", 5f);
+	}
+	void PerderJuego()
+	{
+		juegoTerminado = true;
+		// Aquí puedes mostrar un mensaje de "Game Over" si quieres
+		Invoke("RegresarAlMenuPrincipal", 5f);
+	}
+
+	void RegresarAlMenuPrincipal()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("MenuPrincipal");
+	}
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void Jugar() {
+		SceneManager.LoadScene ("Nivel1");
+	}
+
+	public  void Salir(){
+		Application.Quit ();
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
